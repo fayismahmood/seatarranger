e95fd5dc80fa087df9196f1364cb2990ba3c13b9
fayismahmood <fmthottathil@gmail.com>
2023-09-07 18:06:36 +0530

    asdfasdfasf

--- src/stps/Generate.svelte
+++ src/stps/Generate.svelte
@@ -6,7 +6,7 @@
   let classes: any[] = JSON.parse(localStorage.getItem("class") || "[{}]")?.map(
     (e: any) => e.no
   );
-let hided:{[key:string]:any}={}
+  let hided: { [key: string]: any } = {};
   function shuffleArray(array: any[]) {
     for (var i = array.length - 1; i > 0; i--) {
       var j = Math.floor(Math.random() * (i + 1));
@@ -28,7 +28,7 @@ let hided:{[key:string]:any}={}
 <div class="flex">
   <div class="printArea p-6 flex flex-col shadow-lg border justify-center">
     {#each rooms as _rm}
-      <div class="p-4 m-auto border relative">
+      <div class="p-4 my-3 border relative">
         <div
           class="absolute -left-12 top-1/2 -rotate-90 text-xl text-center font-bold bg-green-600 text-white px-2"
         >
@@ -62,7 +62,7 @@ let hided:{[key:string]:any}={}
                     }`}
                     class={` bg-indigo-500   text-lg font-bold text-white rounded-sm   h-8 flex `}
                   >
-                    {#if arr && (!hided[arr.cls])}
+                    {#if arr && !hided[arr.cls]}
                       <div class="m-auto">
                         {ClassWithRanked[arr.cls]?.[
                           Arrangements.filter(
@@ -91,10 +91,15 @@ let hided:{[key:string]:any}={}
     <div class="mt-2 p-3">
       <div class="font-bold text-gray-500">Hide Class</div>
       <div class="p-3">
-        {#each classes as e,i}
+        {#each classes as e, i}
           <div class="py-1 border-b">
             <label class="relative inline-flex items-center cursor-pointer">
-              <input type="checkbox" value="" class="sr-only peer" bind:checked={hided[e]} />
+              <input
+                type="checkbox"
+                value=""
+                class="sr-only peer"
+                bind:checked={hided[e]}
+              />
               <div
                 class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-slate-300 dark:peer-focus:ring-slate-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-0.5 after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-slate-600"
               />
@@ -113,19 +118,25 @@ let hided:{[key:string]:any}={}
 <style lang="scss">
   .printArea {
     width: 210mm;
-    height: 297mm;
+   // height: 297mm;
+   height: max-content;
     place-content: baseline;
   }
 
-  @media print
-        {
-            @page {
-                margin-top: 0;
-                margin-bottom: 0;
-            }
-           .np {
-               display: none;
-            }
-            
-        }
+  @media print {
+    @page {
+      margin-top: 0;
+      margin-bottom: 0;
+      padding: 10px;
+    }
+    * {
+      break-inside: avoid;
+    }
+    .printArea{
+      padding: 20px;
+    }
+    .np {
+      display: none;
+    }
+  }
 </style>
